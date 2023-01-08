76b45aba27e4fc69a8c7252943799c550109666d
norahm224 <norah.213113778@gmail.com>
2023-01-08 12:28:52 +0300

    add images, views, colors

---
+++ Note_Team/LanuchView.swift
@@ -0,0 +1,20 @@
+//
+//  LanuchView.swift
+//  Note_Team
+//
+//  Created by Nourah Almusaad on 08/01/2023.
+//
+
+import SwiftUI
+
+struct LanuchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LanuchView_Previews: PreviewProvider {
+    static var previews: some View {
+        LanuchView()
+    }
+}
